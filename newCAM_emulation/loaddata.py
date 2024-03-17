ef52ea0c4884444c4515124190a5527efc11265a
Surbhi Goel <surbhigoel77@github.com>
2024-03-17 22:59:57 +0000

    created convection subfolder in demodata and moving reading of data files to loaddata from train.py

--- newCAM_emulation/loaddata.py
+++ newCAM_emulation/loaddata.py
@@ -43,3 +43,14 @@ def data_loader (U,V,T, DSE, NM, NETDT, Z3, RHOI, PS, lat, lon, UTGWSPEC, VTGWSP
 
   return x_train,y_train
 
+
+"""Read the data and the corresponding mean and std deviation"""
+"""Iterating through the data files"""
+s_list = list(range(1, 6))
+
+for iter in s_list:
+    filename = "Demodata/Convection/newCAM_demo_sub_" + str(iter).zfill(1) + ".nc"  # data file
+    print('working on: ', filename)
+    fm = np.load('Demodata/mean_demo_sub.npz')  # mean file
+    fs = np.load('Demodata/std_demo_sub.npz')   # std deviation file
+
